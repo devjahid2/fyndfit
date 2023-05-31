4ce1b2f64436ab138ae9a74c4c58e3031c092db3
Jahid <jahidhasan.developer@gmail.com>
2023-05-31 07:53:05 +0700

    update

---
+++ data.js
@@ -0,0 +1,9 @@
+// all text content provided from here (it will changed on language)
+
+export const ENGLISH = {
+    header:{
+        title:'Looking for an exercise or an equipment?',
+        subtitle:'FyndFit App helps you find exercises based on the equipment, muscle target or exercise categories.',
+        image:'/images/english/'
+    }
+}
